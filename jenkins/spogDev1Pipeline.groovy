f3dfeab0f4e03011c0c17cf185c659d2eba12af0
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-23 22:18:46 -0800

    fix

--- jenkins/spogDev1Pipeline.groovy
+++ jenkins/spogDev1Pipeline.groovy
@@ -3,18 +3,20 @@
 node('nodejs') {
 
 	def config = [
-	       gitRepoUrl: "git@github.com:tarunaz/spog-ui.git",
 	       microservice: "tarun-spog",
+	       version = "1.7"
+	       gitRepoUrl: "git@github.com:tarunaz/spog-ui.git",
 	       sourceRepositoryRef: "master",
 	       gitPullSecret: "tarun-spog-master",
 	       buildNamespace: "nss",
 	       deployNamespace: "tarun-spog1",
-	       emailAddress: "test@example.com"
+	       emailAddress: "test@example.com",
+               ocpUrl = "https://tpaas-console.netapp.com:8443"
 	]
 	
 	stage('Checkout OCP templates') {
 	   echo 'Checkout OpenShift templates'
-	  // checkoutOcpTemplates 'git@github.com:tarunaz/pengg-openshift.git'
+	   checkoutOcpTemplates 'git@github.com:tarunaz/pengg-openshift.git'
 
 	}
 
@@ -24,49 +26,31 @@ node('nodejs') {
 	   
 	}
 
-
 	stage('Start OpenShift build') {
 	   echo 'Start OpenShift build'
-           startOcpBuild {
-	      namespace = "nss"
-	      buildConfigName = "tarun-spog-master"
-	   }
-	   
+           startOcpBuild ( config )
 	}
 
 	stage('Process OCP Deployment templates') {
 	   echo 'Process OpenShift Deployment templates'
-           processOcpDeploymentTemplates {
-               ocpUrl = "https://tpaas-console.netapp.com:8443"
-               namespace = "tarun-spog1"
-	       microservice = "tarun-spog"
-	       sourceRepositoryRef = "master"
-	   }
+           processOcpDeploymentTemplates ( config )
 	}
 
 	stage('Tag OpenShift Image') {
 	   echo 'Tag OpenShift Image'
-           tagOcpImage   
+           tagOcpImage ( config )
 	}
 
 	
 	stage('Import OpenShift Image to TPAAS') {
 	   echo 'Import OpenShift Image to TPAAS'
-           importImageToTpaas {
- 		namespace = "tarun-spog1"
-	        version = '1.7'
-	   }
+           importImageToTpaas ( config )
 	}
 
 	stage('Deploy to TPAAS') {
 	   echo 'Deploy to TPAAS'
-           deployToTpaas {
-               ocpUrl = "https://tpaas-console.netapp.com:8443"
-               namespace = "tarun-spog1"
-	       microservice = "tarun-spog"
-	   }
+           deployToTpaas ( config )
 	}
-
 }
 
 
