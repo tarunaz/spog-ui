950cacd38587d9c4225a20b190bf7d541f0d26b4
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-24 18:54:11 -0800

    fix

--- jenkins/spogDev1Pipeline.groovy
+++ jenkins/spogDev1Pipeline.groovy
@@ -1,55 +1,43 @@
 @Library('pengg-openshift-pipelines') _
 
 node('nodejs') {
-
-	def config = [
-	       microservice: "${BASE}",
-	       version: "1.7",
-	       gitRepoUrl: "${SOURCE_REPOSITORY_URL}",
-	       sourceRepositoryRef: "master",
-	       gitPullSecret: "tarun-spog-master",
-	       buildNamespace: "nss",
-	       deployNamespace: "tarun-spog1",
-	       emailAddress: "test@example.com",
-               ocpUrl: "https://tpaas-console.netapp.com:8443"
-	]
 	
 	stage('Checkout OCP templates') {
 	   echo 'Checkout OpenShift templates'
-	   checkoutOcpTemplates 'git@github.com:tarunaz/pengg-openshift.git'
+	   checkoutOcpTemplates
 
 	}
 
 	stage('Process OCP Build templates') {
 	   echo 'Process OpenShift Build templates'
-           processOcpBuildTemplates ( config )
+           processOcpBuildTemplates
 	   
 	}
 
 	stage('Start OpenShift build') {
 	   echo 'Start OpenShift build'
-           startOcpBuild ( config )
+           startOcpBuild
 	}
 
 	stage('Process OCP Deployment templates') {
 	   echo 'Process OpenShift Deployment templates'
-           processOcpDeploymentTemplates ( config )
+           processOcpDeploymentTemplates
 	}
 
 	stage('Tag OpenShift Image') {
 	   echo 'Tag OpenShift Image'
-           tagOcpImage ( config )
+           tagOcpImage
 	}
 
 	
 	stage('Import OpenShift Image to TPAAS') {
 	   echo 'Import OpenShift Image to TPAAS'
-           importImageToTpaas ( config )
+           importImageToTpaas
 	}
 
 	stage('Deploy to TPAAS') {
 	   echo 'Deploy to TPAAS'
-           deployToTpaas ( config )
+           deployToTpaas
 	}
 }
 
