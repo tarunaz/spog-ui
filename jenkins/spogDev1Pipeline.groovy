2b4c182caabf2153668ef1d93cea3f06160ed176
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-25 09:46:30 -0800

    fix

--- jenkins/spogDev1Pipeline.groovy
+++ jenkins/spogDev1Pipeline.groovy
@@ -3,40 +3,40 @@
 node('nodejs') {
 	
 	stage('Checkout OCP templates') {
-	   echo 'Checkout OpenShift templates'
+
 	   checkoutOcpTemplates()
 
 	}
 
 	stage('Process OCP Build templates') {
-	   echo 'Process OpenShift Build templates'
+	   
            processOcpBuildTemplates()
 	   
 	}
 
-	/*stage('Start OpenShift build') {
-	   echo 'Start OpenShift build'
+	stage('Start OpenShift build') {
+	   
            startOcpBuild()
-	}*/
+	}
 
 	stage('Process OCP Deployment templates') {
-	   echo 'Process OpenShift Deployment templates'
+	   
            processOcpDeploymentTemplates()
 	}
 
 	stage('Tag OpenShift Image') {
-	   echo 'Tag OpenShift Image'
+	   
            tagOcpImage()
 	}
 
 	
 	stage('Import OpenShift Image to TPAAS') {
-	   echo 'Import OpenShift Image to TPAAS'
+	   
            importImageToTpaas()
 	}
 
 	stage('Deploy to TPAAS') {
-	   echo 'Deploy to TPAAS'
+	   
            deployToTpaas()
 	}
 }
